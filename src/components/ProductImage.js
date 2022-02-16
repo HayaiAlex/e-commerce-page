33470692a1e9fdac7f8b5f0375c9e6d0842ba543
hayaialex <alexadams2084@gmail.com>
2022-02-16 12:14:22 +0000

    Added lightbox styling

--- src/components/ProductImage.js
+++ src/components/ProductImage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PreviousIcon from './../images/icon-previous.svg';
 import NextIcon from './../images/icon-next.svg';
+import CloseIcon from './../images/icon-close.svg';
 import ProductImg1 from './../images/image-product-1.jpg';
 import ProductImg2 from './../images/image-product-2.jpg';
 import ProductImg3 from './../images/image-product-3.jpg';
@@ -22,7 +23,7 @@ const ProductImage = () => {
         setActiveImg(eval('img' + img));
     }
 
-    const [tapePosition, setTapePosition] = useState(0)
+    const [tapePosition, setTapePosition] = useState(1)
     const slide = direction => () => {
         console.log("slide", direction, tapePosition)
         let maxImages = 4
@@ -33,6 +34,15 @@ const ProductImage = () => {
         }
     }
 
+    const [lightboxOpen, setLightboxOpen] = useState(false)
+    const toggleLightbox = () => {
+        setLightboxOpen(!lightboxOpen);
+    }
+
+    const setLightboxImage = position => () => {
+        setTapePosition(position)
+    }
+
     return (
         <div id="product-image-container">
 
@@ -40,15 +50,15 @@ const ProductImage = () => {
             <img onClick={slide("left")} id="previous-button" role="button" src={PreviousIcon} alt="Previous Button" />
             <img onClick={slide("right")} id="next-button" role="button" src={NextIcon} alt="Next Button" />
 
-            <div className={'product-image-tape-position-' + tapePosition} id="product-image-tape">
-                <img role="img" src={activeImg} alt="Image of shoes" className="product-image-mobile" />
-                <img role="img" src={activeImg} alt="Image of shoes" className="product-image-mobile" />
-                <img role="img" src={activeImg} alt="Image of shoes" className="product-image-mobile" />
-                <img role="img" src={activeImg} alt="Image of shoes" className="product-image-mobile" />
+            <div className={('product-image-tape-position-' + tapePosition) + " product-image-tape"}>
+                <img role="img" src={ProductImg1} alt="Image of shoes" className="product-image-mobile" />
+                <img role="img" src={ProductImg2} alt="Image of shoes" className="product-image-mobile" />
+                <img role="img" src={ProductImg3} alt="Image of shoes" className="product-image-mobile" />
+                <img role="img" src={ProductImg4} alt="Image of shoes" className="product-image-mobile" />
             </div>
 
             {/* For desktop, use thumbnails under main image */}
-            <img id="product-image" role="img" src={activeImg} alt="Image of shoes" />
+            <img onClick={toggleLightbox} id="product-image" role="img" src={activeImg} alt="Image of shoes" />
 
             <div id="product-image-thumbnail-container">
                 <img onClick={setImage(1)} className={'product-image-thumbnail ' + (activeImg === img1 ? 'active-thumbnail' : '')} src={Thumbnail1} alt="Shoe Thumbnail" />
@@ -56,8 +66,39 @@ const ProductImage = () => {
                 <img onClick={setImage(3)} className={'product-image-thumbnail ' + (activeImg === img3 ? 'active-thumbnail' : '')} src={Thumbnail3} alt="Shoe Thumbnail" />
                 <img onClick={setImage(4)} className={'product-image-thumbnail ' + (activeImg === img4 ? 'active-thumbnail' : '')} src={Thumbnail4} alt="Shoe Thumbnail" />
             </div>
-        </div>
+
+            {/* Desktop lightbox */}
+            <div className={"product-image-lightbox " + (lightboxOpen ? 'product-image-lightbox-open' : '')}>
+                <img onClick={toggleLightbox} role="button" className='close-button' src={CloseIcon} alt="Close Icon" />
+
+                <img onClick={slide("left")} id="previous-button" role="button" src={PreviousIcon} alt="Previous Button" />
+                <img onClick={slide("right")} id="next-button" role="button" src={NextIcon} alt="Next Button" />
+
+                <div className="overflow-hidden">
+                    <div className={'product-image-tape product-image-tape-position-' + tapePosition}>
+                        <img role="img" src={ProductImg1} alt="Image of shoes" className="product-image-mobile" />
+                        <img role="img" src={ProductImg2} alt="Image of shoes" className="product-image-mobile" />
+                        <img role="img" src={ProductImg3} alt="Image of shoes" className="product-image-mobile" />
+                        <img role="img" src={ProductImg4} alt="Image of shoes" className="product-image-mobile" />
+                    </div>
+                </div>
+
+                <div id="product-image-thumbnail-container">
+                    <img onClick={setLightboxImage(1)} className={'product-image-thumbnail ' + (tapePosition === 1 ? 'active-thumbnail' : '')} src={Thumbnail1} alt="Shoe Thumbnail" />
+                    <img onClick={setLightboxImage(2)} className={'product-image-thumbnail ' + (tapePosition === 2 ? 'active-thumbnail' : '')} src={Thumbnail2} alt="Shoe Thumbnail" />
+                    <img onClick={setLightboxImage(3)} className={'product-image-thumbnail ' + (tapePosition === 3 ? 'active-thumbnail' : '')} src={Thumbnail3} alt="Shoe Thumbnail" />
+                    <img onClick={setLightboxImage(4)} className={'product-image-thumbnail ' + (tapePosition === 4 ? 'active-thumbnail' : '')} src={Thumbnail4} alt="Shoe Thumbnail" />
+                </div>
+            </div>
+        </div >
     );
 };
 
 export default ProductImage;
+
+// TODO:
+// make lightbox thumbnails not control main product image
+// make lightbox thumbnails control lightbox image
+// when arrows used in lightbox, they must change thumbnail highlight
+// check height so lightbox fills screen more on the bottom
+// add transition to lightbox-open
